ca71b1958922cadfbd4f496a312822e5dbc7d235
Carlos <carlosoftwar@gmail.com>
2021-02-24 02:40:29 -0300

    Update : Added Router User

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import "reflect-metadata";
+import express from 'express';
+import "./database";
+import {router} from './routers';
+
+const app = express();
+
+app.use(express.json())
+app.use(router);
+
+
+app.listen(3030, () => {
+    console.log("App Runing!");
+})
