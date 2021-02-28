4fafd4163f2e9a8738cfaa59a0e253a452a0d16c
Carlos <carlosoftwar@gmail.com>
2021-02-27 21:14:10 -0300

    Update : user controller

---
+++ src/repositories/UsersRepository.ts
@@ -0,0 +1,10 @@
+
+import { Repository } from "./repository/Repository";
+import {User} from "../models/user";
+
+class UsersRepository extends Repository <User>{
+
+    
+}
+
+export { UsersRepository }
